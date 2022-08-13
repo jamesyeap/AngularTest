00b4a4a896d60a23eb86009299ed6de2ef045f5d
James Yeap <jamesyeap.ys@gmail.com>
2022-08-13 11:12:49 +0800

    Add input text box and markdown toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import '@github/markdown-toolbar-element';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularTest';
+  value = 0;
+  message!: string;
+
+  commentForm: FormGroup;
+
+  constructor() {
+    const textValue: FormControl = new FormControl('');
+    this.commentForm = new FormGroup({ textValue });
+  }
 }
